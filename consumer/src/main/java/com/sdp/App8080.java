a83d8628baf65414d48d6c12a7fd26a5896d5bee
王茂玲 <wangmaoling@wangmaolingdeMacBook-Pro.local>
2020-11-23 22:51:08 +0800

    初始化

---
+++ consumer/src/main/java/com/sdp/App8080.java
@@ -0,0 +1,11 @@
+package com.sdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App8080 {
+    public static void main(String[] args) {
+        SpringApplication.run(App8080.class, args);
+    }
+}
---
+++ consumer/src/main/java/com/sdp/component/MsgReceiver.java
@@ -0,0 +1,14 @@
+package com.sdp.component;
+
+
+import org.springframework.stereotype.Component;
+
+
+/**
+ * 消息接受者
+ */
+@Component
+public class MsgReceiver {
+
+
+}
